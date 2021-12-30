0bff6d42103b0deb79684b5a648135c6be57c459
Satheeshkumar Rajagopal <35360464+RSK14@users.noreply.github.com>
2021-12-30 22:05:10 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,8 +33,7 @@ aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
 #aws s3 cp ${FILE} s3://${S3_BUCKET}/${FILE} --recursive
-aws s3 cp test30 s3://dev2.business.appointos.com/test30 --recursive
-
+aws s3 cp test30 s3://dev2.business.appointos.com/test30 --recursive --region ${AWS_REGION} $*
 rm -rf ~/.aws
 
 
