5a40b3d4bffb7b02824d47fe343718c38a74d730
Satheeshkumar Rajagopal <35360464+RSK14@users.noreply.github.com>
2022-02-17 10:32:38 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,14 +24,13 @@ set -e
 #   AWS_REGION="us-east-1"
 # fi
 
-	mkdir -p ~/.aws
-	touch ~/.aws/credentials
-	echo "[default]
-	#aws_secret_cloudfront_distributtion_id = ${AWS_SECRET_CLOUDFRONT_DISTRIBUTION_ID}
-	aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-	aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
-	aws s3 cp ${FILE_QA} s3://${S3_BUCKET_QA}/${FILE_QA} --recursive 
-	aws s3 cp ${FILE_PROD} s3://${S3_BUCKET_PROD}/${FILE_PROD} --recursive
-	rm -rf ~/.aws
-
+#	mkdir -p ~/.aws
+#	touch ~/.aws/credentials
+#	echo "[default]
+#	#aws_secret_cloudfront_distributtion_id = ${AWS_SECRET_CLOUDFRONT_DISTRIBUTION_ID}
+#	aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+#	aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
+#	aws s3 cp ${FILE_QA} s3://${S3_BUCKET_QA}/${FILE_QA} --recursive 
+#	aws s3 cp ${FILE_PROD} s3://${S3_BUCKET_PROD}/${FILE_PROD} --recursive
+#	rm -rf ~/.aws
 
